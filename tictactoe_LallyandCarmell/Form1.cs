83a408af431254c2f3f64fb57e149ad632375ae5
Carmellw <carmellwass@gmail.com>
2020-11-19 16:29:16 +0200

    a

--- tictactoe_LallyandCarmell/Form1.cs
+++ tictactoe_LallyandCarmell/Form1.cs
@@ -16,5 +16,10 @@ namespace tictactoe_LallyandCarmell
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
