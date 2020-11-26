4de932399f46a746d9860d14c364395de212ca48
Carmellw <carmellwass@gmail.com>
2020-11-26 16:57:50 +0200

    4.2

--- tictactoe_LallyandCarmell/Form1.Designer.cs
+++ tictactoe_LallyandCarmell/Form1.Designer.cs
@@ -28,139 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
-            this.pictureBox9 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.pictureBox00 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox01 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox02 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBox00
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(22, 25);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox00.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox00.ImageLocation = "";
+            this.pictureBox00.Location = new System.Drawing.Point(22, 25);
+            this.pictureBox00.Name = "pictureBox00";
+            this.pictureBox00.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox00.TabIndex = 0;
+            this.pictureBox00.TabStop = false;
+            this.pictureBox00.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox2
+            // pictureBox10
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(321, 320);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pictureBox10.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox10.Location = new System.Drawing.Point(321, 25);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 1;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox3
+            // pictureBox20
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(22, 320);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.pictureBox20.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox20.Location = new System.Drawing.Point(619, 25);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox20.TabIndex = 2;
+            this.pictureBox20.TabStop = false;
+            this.pictureBox20.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox4
+            // pictureBox01
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(619, 25);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
+            this.pictureBox01.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox01.Location = new System.Drawing.Point(22, 320);
+            this.pictureBox01.Name = "pictureBox01";
+            this.pictureBox01.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox01.TabIndex = 3;
+            this.pictureBox01.TabStop = false;
+            this.pictureBox01.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox5
+            // pictureBox11
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(321, 25);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox5.TabIndex = 4;
-            this.pictureBox5.TabStop = false;
+            this.pictureBox11.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox11.Location = new System.Drawing.Point(321, 320);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 4;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox6
+            // pictureBox12
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(619, 613);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox6.TabIndex = 5;
-            this.pictureBox6.TabStop = false;
+            this.pictureBox12.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox12.Location = new System.Drawing.Point(619, 320);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 5;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox7
+            // pictureBox02
             // 
-            this.pictureBox7.Location = new System.Drawing.Point(619, 320);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox7.TabIndex = 6;
-            this.pictureBox7.TabStop = false;
+            this.pictureBox02.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox02.Location = new System.Drawing.Point(22, 618);
+            this.pictureBox02.Name = "pictureBox02";
+            this.pictureBox02.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox02.TabIndex = 6;
+            this.pictureBox02.TabStop = false;
+            this.pictureBox02.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox8
+            // pictureBox21
             // 
-            this.pictureBox8.Location = new System.Drawing.Point(22, 613);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox8.TabIndex = 7;
-            this.pictureBox8.TabStop = false;
+            this.pictureBox21.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox21.Location = new System.Drawing.Point(321, 618);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox21.TabIndex = 7;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
-            // pictureBox9
+            // pictureBox22
             // 
-            this.pictureBox9.Location = new System.Drawing.Point(321, 613);
-            this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(280, 274);
-            this.pictureBox9.TabIndex = 8;
-            this.pictureBox9.TabStop = false;
+            this.pictureBox22.Image = global::tictactoe_LallyandCarmell.Properties.Resources.white;
+            this.pictureBox22.Location = new System.Drawing.Point(619, 618);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(280, 274);
+            this.pictureBox22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox22.TabIndex = 8;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Click += new System.EventHandler(this.pictureBox_Box_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(931, 913);
-            this.Controls.Add(this.pictureBox9);
-            this.Controls.Add(this.pictureBox8);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(911, 913);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox02);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox01);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox00);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox8;
-        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox00;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox01;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox02;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox22;
     }
 }
 
--- tictactoe_LallyandCarmell/Form1.cs
+++ tictactoe_LallyandCarmell/Form1.cs
@@ -7,19 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using tictactoe_LallyandCarmell.Properties;
+
 
 namespace tictactoe_LallyandCarmell
 {
     public partial class Form1 : Form
     {
+        private bool isPlayer1 = true;
+        private Image[] m_Images = new Image[3]; //המערך של התמונות
+
         public Form1()
         {
             InitializeComponent();
+            SetImagesArray();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        public void SetImagesArray()
+        {
+            m_Images[0] = Resources.white;
+            m_Images[1] = Resources.x;
+            m_Images[2] = Resources.circle;
+        }
+
+        private void pictureBox_Box_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+
+            if (isPlayer1)
+            {
+                if (pictureBox.Image != m_Images[1] && pictureBox.Image != m_Images[2])
+                {
+                    pictureBox.Image = m_Images[1];
+                    isPlayer1 = false;
+                }
+
+                else
+                {
+                    MessageBox.Show("choose an empty space");
+                }
+            }
+
+            else
+            {
+                if (pictureBox.Image != m_Images[1] && pictureBox.Image != m_Images[2])
+                {
+                    pictureBox.Image = m_Images[2];
+                    isPlayer1 = true;
+                }
+
+                else
+                {
+                    MessageBox.Show("choose an empty space");
+                }
+            }
+
+        }
+
+        public string[,] FormToMatrix()
         {
 
+            //המטריצה המוחזרת
+
+            string[,] m = new string[3, 3];
+            int row, col;
+            PictureBox curPictureBox;
+
+            //עוברים על כלל הפקדים בטופס - נעזרים בלולאת foreach
+
+            foreach (Control ctrl in this.Controls)
+                if (ctrl is PictureBox)
+                {
+                    curPictureBox = ctrl as PictureBox;
+
+                    //מציאת מספר השורה והעמודה מתוך שם הפקד
+
+                    row = int.Parse(curPictureBox.Name[curPictureBox.Name.Length - 2].ToString());
+                    col = int.Parse(curPictureBox.Name[curPictureBox.Name.Length - 1].ToString());
+                    m[row, col] = curPictureBox.Text;
+
+
+                }
+            return m;
+        }
+
+        public string WhatIsPicture(PictureBox pictureBox)
+        {
+            if (pictureBox.Image == m_Images[1])
+            {
+                return ("X");
+            }
+
+            if (pictureBox.Image == m_Images[2])
+            {
+                return ("O");
+            }
+
+            return ("white");
         }
     }
 }
